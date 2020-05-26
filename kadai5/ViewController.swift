e47ebce96f4b67e61902eb3f10ca8ccf7b9ec0d6
shogo <ujiiiiie@gmail.com>
2020-05-26 09:37:40 +0900

    Initial Commit

---
+++ kadai5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kadai5
+//
+//  Created by USER on 2020/05/26.
+//  Copyright © 2020 shogo.ujiie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
